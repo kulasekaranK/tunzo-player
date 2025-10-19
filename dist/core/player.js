959248e01aae1739121e6ac0fd7ed48d76c14824
Guna <itzkulasekaran@gmail.com>
2025-10-19 21:53:50 +0000

    Refactor package.json scripts formatting and add comment in Player class for clarity

--- dist/core/player.js
+++ dist/core/player.js
@@ -14,6 +14,7 @@ class Player {
         this.audio.load();
         this.audio.play().catch(() => { });
     }
+    //updated
     static play(song, index = 0) {
         var _a;
         if (!song || !song.downloadUrl)
